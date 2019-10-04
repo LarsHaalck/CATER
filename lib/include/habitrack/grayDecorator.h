2cd18c34ffcfc61654862c1e293cb93657674702
Lars Haalck <lars.haalck@uni-muenster.de>
2019-10-04 15:34:51 +0200

    adds gray decorator

---
+++ lib/include/habitrack/grayDecorator.h
@@ -0,0 +1,16 @@
+#ifndef HABITRACK_GRAY_DECORATOR_H
+#define HABITRACK_GRAY_DECORATOR_H
+
+#include "habitrack/baseDecorator.h"
+
+namespace ht
+{
+class GrayDecorator : public BaseDecorator
+{
+public:
+    GrayDecorator(std::shared_ptr<ImageContainer> baseContainer);
+    cv::Mat at(std::size_t idx) const override;
+};
+} // namespace ht
+#endif // HABITRACK_GRAY_DECORATOR_H
+
