0165373437fee45bddaf22833cfa82048143ef8f
Lars Haalck <lars.haalck@uni-muenster.de>
2019-10-16 17:47:27 +0200

    adds progressbar

--- lib/main.cpp
+++ lib/main.cpp
@@ -8,17 +8,23 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 
+#include "MILD/loop_closure_detector.h"
+
 using namespace ht;
 int main()
 {
-    std::size_t cacheSize = 10;
+    std::size_t cacheSize = 200;
 
     auto container = std::make_shared<ImageContainer>(
-        "/home/lars/data/ontogenyTest/vid1/imgs");
+        "/home/lars/data/ontogenyTest/vid3/imgs");
 
     auto ftContainer = std::make_shared<FeatureContainer>(
-        container, "/home/lars/data/ontogenyTest/vid1/fts", FeatureType::ORB, 5000);
-    ftContainer->compute(true, cacheSize);
+        container, "/home/lars/data/ontogenyTest/vid3/fts", FeatureType::ORB, 5000);
+    ftContainer->compute(cacheSize, false);
+
+
+    MILD::LoopClosureDetector lcd(FEATURE_TYPE_ORB, 16, 0);
+    lcd.displayParameters();
 
     return 0;
 }
---
+++ lib/src/progressBar.h
@@ -0,0 +1,80 @@
+#ifndef PROGRESS_BAR_H
+#define PROGRESS_BAR_H
+
+#include <chrono>
+#include <iostream>
+
+namespace ht
+{
+class ProgressBar
+{
+public:
+    ProgressBar(unsigned int total, unsigned int width)
+        : mTotalTicks(total)
+        , mBarWidth(width)
+    {
+    }
+
+    ProgressBar(unsigned int total)
+        : mTotalTicks(total)
+    {
+    }
+
+    void operator++() { mTicks++; }
+
+    void display() const
+    {
+        auto progress = static_cast<float>(mTicks) / mTotalTicks;
+        auto pos = static_cast<unsigned int>(mBarWidth * progress);
+
+        std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+        auto time_elapsed
+            = std::chrono::duration_cast<std::chrono::milliseconds>(now - start_time)
+                  .count();
+
+        std::cout << "[";
+
+        for (unsigned int i = 0; i < mBarWidth; ++i)
+        {
+            if (i < pos)
+                std::cout << mCompleteChar;
+            else if (i == pos)
+                std::cout << ">";
+            else
+                std::cout << mInCompleteChar;
+        }
+        std::cout << "] " << static_cast<int>(progress * 100.0) << "% "
+            << static_cast<float>(time_elapsed) / 1000.0 << "s\r";
+        std::cout.flush();
+    }
+
+    ~ProgressBar()
+    {
+        done();
+    }
+
+    void done()
+    {
+        if (!mIsFinished)
+        {
+            display();
+            std::cout << std::endl;
+            mIsFinished = true;
+        }
+    }
+
+private:
+    unsigned int mTicks = 0;
+
+    const unsigned int mTotalTicks;
+    const unsigned int mBarWidth = 60;
+    const char mCompleteChar = '=';
+    const char mInCompleteChar = ' ';
+    const std::chrono::steady_clock::time_point start_time
+        = std::chrono::steady_clock::now();
+
+    bool mIsFinished = false;
+
+};
+} // namespace ht
+#endif // PROGRESS_BAR_H
