713c1970a56d553ffba19b231177132ba927e145
Lars Haalck <l_haal02@uni-muenster.de>
2019-10-27 18:23:50 +0100

    adds example calls in example main.cpp

--- lib/main.cpp
+++ lib/main.cpp
@@ -12,33 +12,54 @@
 using namespace ht;
 int main()
 {
-    std::size_t cacheSize = 5000;
+    std::size_t cacheSize = 50;
     auto path = std::string("/home/lars/data/ontogenyTest/vid3");
 
+    // load images
     auto imgContainer = std::make_shared<ImageContainer>(path + "/imgs");
 
+    // calc features
     auto ftContainer
-        = std::make_shared<FeatureContainer>(imgContainer, path + "/fts", FeatureType::SIFT, 3000);
+        = std::make_shared<FeatureContainer>(imgContainer, path + "/fts", FeatureType::SIFT, 5000);
     ftContainer->compute(cacheSize, ComputeBehavior::Keep);
 
-    /* auto keyFrameSelector = std::make_unique<KeyFrameSelector>(ftContainer); */
-    /* auto keyFrames = keyFrameSelector->compute(0.3, 0.75); */
+    // select key frames
+    auto keyFrameSelector
+        = std::make_unique<KeyFrameSelector>(ftContainer, path + "/key_frames.yml");
+    auto keyFrames = keyFrameSelector->compute(0.3, 0.75, ComputeBehavior::Keep);
 
-    /* for (auto kf : keyFrames) */
-    /* { */
-    /*     std::cout << kf << std::endl; */
-    /*     cv::imshow("hi", imgContainer->at(kf)); */
-    /*     cv::waitKey(0); */
-    /* } */
-
-    /* std::cout << std::endl; */
-
+    // do (exhaustive|mild) matching on key frames only
     auto matchContainer
-        = std::make_shared<MatchesContainer>(ftContainer, path + "/matches", MatchType::MILD, 10,
-            GeometricType::Homography | GeometricType::Affinity | GeometricType::Similarity);
+        = std::make_shared<MatchesContainer>(ftContainer, path + "/kfs", MatchType::MILD, 10,
+            GeometricType::Homography | GeometricType::Affinity | GeometricType::Similarity
+                | GeometricType::Isometry);
+    matchContainer->compute(5000, ComputeBehavior::Keep, keyFrames);
 
-    matchContainer->compute(cacheSize, ComputeBehavior::Overwrite);
-    /* matchContainer->compute(cacheSize, false); */
+    GeometricType useableTypes = matchContainer->getUsableTypes(keyFrames);
+
+    auto typeList = typeToTypeList(useableTypes);
+    for (auto type : typeList)
+    {
+        std::cout << type << std::endl;
+    }
+
+    /* auto matches = matchContainer->getTrafos(GeometricType::Affinity); */
+    /* std::cout << matches.size() << std::endl; */
+
+    // do pano stitching for every wanted type
+    /* auto stitcher = std::make_unique<PanoramaSticher>(imgContainer, ftContainer, matchContainer);
+     */
+    /* stitcher->initTrafos(); */
+    /* auto panoImg0 = sticher->stitchPano(); */
+
+    /* sticher->globalOptimize(); */
+    /* auto panoImg1 = sticher->stitchPano(); */
+    /* auto trafoList = sticher->getTrafoList(); */
+
+    // keep keyframe transformation constant
+    // init non-keyframes by interpolating
+    // bundleadjust
+    /* sticher->reintegrate(); */
 
     return 0;
 }
