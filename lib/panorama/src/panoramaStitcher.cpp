ca226fe26af9d26d38061d59b222a0fd16f0d967
Lars Haalck <lars.haalck@uni-muenster.de>
2020-12-08 08:08:49 +0100

    More verbose stitching

--- lib/panorama/src/panoramaStitcher.cpp
+++ lib/panorama/src/panoramaStitcher.cpp
@@ -343,10 +343,28 @@ std::tuple<cv::Mat, cv::Mat, cv::Mat> PanoramaStitcher::stitchPano(cv::Size targ
                 tcm_color = tcm::GetColor(
                     static_cast<double>(i) / (centersTrans.size() - 1), Viridis);
             }
+            /* tcm_color = tcm::GetColor(0.5, Viridis); // greenish */
+            tcm_color = tcm::GetColor(1.0, Viridis); // yellow
             auto color = cv::Scalar(tcm_color.b() * 255, tcm_color.g() * 255, tcm_color.r() * 255);
 
             if (i > 0)
                 cv::line(pano, centersTrans[i - 1], centersTrans[i], color, 4);
+
+            auto marker = cv::MarkerTypes::MARKER_SQUARE;
+            if (isKeyFrame(i))
+            {
+                marker = cv::MarkerTypes::MARKER_DIAMOND;
+                tcm_color = tcm::GetColor(0.0, Viridis);
+                color = cv::Scalar(tcm_color.b() * 255, tcm_color.g() * 255, tcm_color.r() * 255);
+                cv::drawMarker(pano, centersTrans[i], color, marker, 15, 2, cv::LineTypes::FILLED);
+            }
+            /* else if (i % 1 == 0) */
+            /* { */
+            /*     marker = cv::MarkerTypes::MARKER_CROSS; */
+            /*     tcm_color = tcm::GetColor(1.0, Viridis); */
+            /*     color = cv::Scalar(tcm_color.b() * 255, tcm_color.g() * 255, tcm_color.r() * 255); */
+            /*     cv::drawMarker(pano, centersTrans[i], color, marker, 15, 2, cv::LineTypes::FILLED); */
+            /* } */
         }
 
         // DEBUG
