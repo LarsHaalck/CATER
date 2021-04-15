9a69a15b824de35f0b08a5cbf9d06b9c3ed97d1f
Lars Haalck <lars.haalck@uni-muenster.de>
2021-04-15 16:54:31 +0200

    Add fitpack as dependency

---
+++ super/external/fitpackpp.cmake
@@ -0,0 +1,28 @@
+find_package(fitpackpp CONFIG QUIET)
+
+if(TARGET fitpackpp::fitpackpp)
+    get_property(loc TARGET fitpackpp::fitpackpp PROPERTY LOCATION)
+    message(STATUS "Found fitpackpp at ${loc}")
+    add_library(fitpackpp_external INTERFACE)
+else()
+    message(STATUS "fitpackpp could not be located. Building from source...")
+    include(ExternalProject)
+    ExternalProject_Add(
+        fitpackpp_external
+        GIT_REPOSITORY https://github.com/LarsHaalck/fitpackpp
+        GIT_TAG 14fac733d7c5df83f60657610331fee82fbecff3
+        CMAKE_ARGS
+          -DCMAKE_INSTALL_PREFIX=${STAGED_INSTALL_PREFIX}
+          -DCMAKE_BUILD_TYPE=Release
+          -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+          -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+          -DBUILD_EXAMPLES=OFF
+        LOG_DOWNLOAD ON
+        LOG_UPDATE ON
+        LOG_PATCH ON
+        LOG_CONFIGURE ON
+        LOG_BUILD ON
+        LOG_INSTALL ON
+    )
+    set(fitpackpp_DIR ${STAGED_INSTALL_PREFIX}/lib/cmake/fitpackpp)
+endif()
