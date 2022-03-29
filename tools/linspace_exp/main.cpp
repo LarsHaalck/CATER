8dba8c9227971d340f92980f5867b0e906590b9f
Lars Haalck <lars.haalck@uni-muenster.de>
2022-03-29 16:53:30 +0200

    Fix a bug in linspace experiment

--- tools/linspace_exp/main.cpp
+++ tools/linspace_exp/main.cpp
@@ -5,10 +5,10 @@
 #include <habitrack/tracker/tracker.h>
 #include <habitrack/tracker/unaries.h>
 
+#include <fstream>
 #include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
-#include <fstream>
 
 #include <spdlog/cfg/env.h>
 #include <spdlog/spdlog.h>
@@ -52,45 +52,66 @@ void write_csv(const ht::Detections& dets, double perc)
     }
 }
 
+std::vector<cv::Point2d> read_gt(const std::filesystem::path& gt_file)
+{
+    std::ifstream stream(gt_file.string());
+    if (!stream.is_open())
+        throw std::runtime_error("Gt file could not be opened");
+
+    std::vector<cv::Point2d> pts;
+    for (std::string line; std::getline(stream, line);)
+    {
+        auto it = line.find(",");
+        auto a = static_cast<double>(std::stod(line.substr(0, it)));
+        auto b = static_cast<double>(std::stod(line.substr(it + 1)));
+        pts.push_back({a, b});
+    }
+    return pts;
+}
+
 using path = std::filesystem::path;
-path base_path = "/data/Dropbox/Selected Ontogeny Data/Videos/Ant13/Ant13R1";
+path base_path = "/data/ant-ml/Ant2ZVF";
 path img_folder = base_path / "imgs";
 path match_folder = base_path / "imgs_output" / "now" / "matches";
 path un_folder = base_path / "imgs_output" / "now" / "unaries";
+path dets_file = base_path / "imgs_output" / "now" / "detections.yml";
 
-std::size_t start_frame = 619;
-std::size_t end_frame = 13724;
+std::size_t start_frame = 0;
+std::size_t end_frame = 3222;
 
 int main()
 {
     spdlog::set_level(spdlog::level::info);
 
-    auto imgs = ht::Images(img_folder);
+    auto imgs = ht::Images(img_folder, ht::Images::ReadMode::Unchanged, cv::Vec3d(), cv::Vec2d(),
+        cv::Rect2i(448, 28, 1024, 1024));
     auto trafos = ht::matches::getTrafos(match_folder, ht::GeometricType::Homography);
     auto uns = ht::Unaries::fromDir(imgs, un_folder, start_frame, end_frame);
-    auto manual_uns = ht::ManualUnaries::fromDir(un_folder, 0.8, 9, imgs.getImgSize());
-    auto settings = ht::Tracker::Settings {0.8, 15, 6.0, 4.0, false, 5, 3, 0};
-    /* auto gt = read_gt(); */
+    auto settings = ht::Tracker::Settings {0.8, 25, 6.0, 4.0, false, 5, 3, 0};
+    auto dets = ht::Detections::fromDir(dets_file);
 
-    double perc = 0.0;
+    double perc = 0.15;
     int num_images = end_frame - start_frame;
     while (perc <= 1)
     {
+        auto manual_uns = ht::ManualUnaries(0.8, 9, imgs.getImgSize());
+        std::cout << "perc: " << perc << std::endl;
         int num = std::round(perc * num_images);
         if (num == 0)
             num = 2;
 
-        auto ids_d = linspace(0, num_images - 1, num);
+        auto ids_d = linspace(start_frame, end_frame - 1, num);
         std::vector<int> ids;
         ids.reserve(ids_d.size());
         std::transform(std::begin(ids_d), std::end(ids_d), std::back_inserter(ids),
             [](double e) { return std::round(e); });
 
-        /* for (auto id : ids) */
-        /*     manual_uns.insert(id, gt[id]); */
+        for (auto id : ids)
+            manual_uns.insert(id, dets.at(id).position);
 
         auto dets = ht::Tracker::track(uns, manual_uns, settings, trafos);
         write_csv(dets, perc);
+        perc += 0.01;
     }
 
     return 0;
