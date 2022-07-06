0ff756988519151c4666501f92e608d4b76da8ce
Lars Haalck <lars.haalck@uni-muenster.de>
2022-07-06 14:22:43 +0200

    Add other trackers to equidistant interact test

--- tools/interact_test/main_eq.cpp
+++ tools/interact_test/main_eq.cpp
@@ -19,8 +19,30 @@
 #include <spdlog/spdlog.h>
 
 using path = std::filesystem::path;
-
 std::size_t chunk = 100;
+
+enum class TrackerType
+{
+    Interpolate = 1,
+    Chunked = 2,
+    Continous = 3,
+
+};
+
+std::string get_suffix(const TrackerType& type)
+{
+    switch (type)
+    {
+    case TrackerType::Interpolate:
+        return "int";
+    case TrackerType::Chunked:
+        return "ch" + std::to_string(chunk);
+    case TrackerType::Continous:
+        return "con";
+    default:
+        return "";
+    };
+}
 
 int main(int argc, char** argv)
 {
@@ -28,9 +50,12 @@ int main(int argc, char** argv)
 
     path res_file, out_dir;
     double step_perc = 1;
-    cxxopts::Options options("Interact-Test [Interpolation]", "");
+    auto trackerType_int = 1;
+    cxxopts::Options options("Interact-Test [Equidistant]", "");
     options.add_options()("r,res_file", "The results file to process", cxxopts::value(res_file))(
-        "s,step", "The step size in percent (default 1)", cxxopts::value(step_perc))("o,out",
+        "s,step", "The step size in percent (default 1)", cxxopts::value(step_perc))("t,tracker",
+        "The tracker type to be used, 1: interpolate, 2: chunked, 3: continous (default 1)",
+        cxxopts::value(trackerType_int))("o,out",
         "The out dir relative to parent of images (default parent path of images)",
         cxxopts::value(out_dir));
 
@@ -40,6 +65,10 @@ int main(int argc, char** argv)
         std::cout << options.help() << std::endl;
         return -1;
     }
+
+    auto trackerType = static_cast<TrackerType>(trackerType_int);
+    if (trackerType != TrackerType::Chunked)
+        chunk = 0;
 
     std::cout << "You called: " << std::endl;
     std::cout << "r: " << res_file.string() << std::endl;
@@ -61,14 +90,14 @@ int main(int argc, char** argv)
     auto gt = ht::Detections::fromDir(detectFile);
     auto trafos = ht::matches::getTrafos(match_folder, ht::GeometricType::Homography);
     auto uns = ht::Unaries::fromDir(imgs, un_folder, start_frame, end_frame);
-    auto settings = ht::InterpTracker::Settings {0.8, 25, 6, 4, false, 5, 3, chunk};
+    auto settings = ht::Tracker::Settings {0.8, 25, 6, 4, false, 5, 3, chunk};
 
     out_dir = base_path / out_dir;
     std::filesystem::create_directories(out_dir);
 
     // max is 10% of all frames
     auto upper_bound = static_cast<std::size_t>(std::ceil(gt.size() / 10.0));
-    std::size_t step = std::round(step_perc/100 * gt.size());
+    std::size_t step = std::round(step_perc / 100 * gt.size());
 
     auto progress = ht::ProgressBar();
     progress.setTotal(upper_bound / step);
@@ -80,10 +109,16 @@ int main(int argc, char** argv)
         for (auto id : ids)
             manual_uns.insert(id, gt.at(id).position);
 
-        spdlog::info("Running InterpTracker with {} manual unaries", manual_uns.size());
-        auto detections = ht::InterpTracker::track(uns, manual_uns, settings, trafos);
-        detections.save(
-            out_dir / ("detections_int_" + std::to_string(manual_uns.size()) + ".yaml"));
+        spdlog::info("Running Tracker with {} manual unaries", manual_uns.size());
+        auto detections = ht::Detections();
+        if (trackerType == TrackerType::Interpolate)
+            detections = ht::InterpTracker::track(uns, manual_uns, settings, trafos);
+        else
+            detections = ht::Tracker::track(uns, manual_uns, settings, trafos);
+
+        detections.save(out_dir
+            / ("detections_eq_" + get_suffix(trackerType) + "_" + std::to_string(manual_uns.size())
+                + ".yaml"));
         progress.inc();
     }
     progress.done();
